d510f5990d2dd5aa8cff67b49b6a0491ba785485
Jochen Schalanda <jochen@schalanda.name>
2014-04-27 19:36:36 +0200

    Add GelfBootstrap to HelloWorldApplication

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -6,7 +6,11 @@ import com.example.helloworld.core.Person;
 import com.example.helloworld.core.Template;
 import com.example.helloworld.db.PersonDAO;
 import com.example.helloworld.health.TemplateHealthCheck;
-import com.example.helloworld.resources.*;
+import com.example.helloworld.resources.HelloWorldResource;
+import com.example.helloworld.resources.PeopleResource;
+import com.example.helloworld.resources.PersonResource;
+import com.example.helloworld.resources.ProtectedResource;
+import com.example.helloworld.resources.ViewResource;
 import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.auth.basic.BasicAuthProvider;
@@ -16,9 +20,19 @@ import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
+import net.gini.dropwizard.gelf.logging.GelfBootstrap;
+import net.gini.dropwizard.gelf.logging.UncaughtExceptionHandlers;
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    private static final String NAME = "hello-world";
+    private static final String GELF_HOST = "localhost";
+    private static final int GELF_PORT = 12201;
+
     public static void main(String[] args) throws Exception {
+        GelfBootstrap.bootstrap(NAME, GELF_HOST, GELF_PORT, false);
+        Thread.currentThread().setUncaughtExceptionHandler(UncaughtExceptionHandlers.systemExit());
+
         new HelloWorldApplication().run(args);
     }
 
@@ -32,7 +46,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 
     @Override
     public String getName() {
-        return "hello-world";
+        return NAME;
     }
 
     @Override
